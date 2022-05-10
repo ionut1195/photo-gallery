4f1791e418cdec9d5d6120ff3ecadd71c7db5d48
ionut1195 <ionut_tomescu95@yahoo.com>
2022-05-10 13:36:28 +0200

    centered display div

--- src/App.js
+++ src/App.js
@@ -37,10 +37,10 @@ const allPictures = [g1,g2,g3,g4,g5,g6,g7,g8 ,n1, n2, n3, n4, n5, n6, n7, spider
 function App() {
 	const [hiddenDisplay, setHiddenDisplay] = useState('hidden')
 	const handleSetHiddenDisplay = () => {
-		setHiddenDisplay('block')
+		setHiddenDisplay('flex')
 	}
 	const toggleHiddenDisplay = () => {
-		setHiddenDisplay(hiddenDisplay === 'hidden' ? 'block': 'hidden')
+		setHiddenDisplay(hiddenDisplay === 'hidden' ? 'flex': 'hidden')
 	}
 	const [imgSource, setImgSource] = useState('')
 	const handleSetImgSource = (st) => {
@@ -63,11 +63,11 @@ function App() {
 	}
 
   return (
-		<div className='relative'>
+		<div className='relative flex flex-col '>
 			<Navbar />
 			<Hero id='hero' />
 			<StickySidebar />
-			<div className={`${hiddenDisplay} sticky container justify-center items-center flex md:top-[10%] backdrop-blur-lg z-10  mx-auto rounded-2xl bg-blue-100 h-[100vh]  md:h-50vh lg:h-[calc(100vh_-_20vh)]`}>
+			<div className={`${hiddenDisplay}  fixed top-0 self-center container justify-center items-center  md:top-[10%] backdrop-blur-lg z-10 rounded-2xl bg-blue-100 h-[100vh]  md:h-50vh lg:h-[calc(100vh_-_20vh)]`}>
 				<FontAwesomeIcon onClick={handleClickLeft} className='absolute cursor-pointer left-0 top-1/2 text-hover-color text-2xl opacity-50 hover:opacity-100' icon={faCircleLeft} />
 				<FontAwesomeIcon onClick={handleClickRight} className='absolute cursor-pointer top-1/2 right-0  text-hover-color text-2xl  opacity-50 hover:opacity-100' icon={faCircleRight} />
 				{console.log(allPictures.indexOf(imgSource))}
