3f92813ba94556c8cd21f1d702884e81148b87a4
ionut1195 <ionut_tomescu95@yahoo.com>
2022-05-12 11:25:37 +0200

    refactored to fetch pictures links from database

--- src/App.js
+++ src/App.js
@@ -4,36 +4,34 @@ import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 import StickySidebar from './components/StickySidebar';
 import HiddenDisplay from './components/HiddenDisplay';
-
-import g1 from './photos/architecture/a1.webp'
-import g2 from './photos/architecture/a2.webp'
-import g3 from './photos/architecture/a3.webp'
-import g4 from './photos/architecture/a4.webp'
-import g5 from './photos/architecture/a5.webp'
-import g6 from './photos/architecture/a6.webp'
-import g7 from './photos/architecture/a7.webp'
-import g8 from './photos/architecture/a8.webp'
-
-import n1 from './photos/nature/n1.webp'
-import n2 from './photos/nature/n2.webp'
-import n3 from './photos/nature/n3.webp'
-import n4 from './photos/nature/n4.webp'
-import n5 from './photos/nature/n5.webp'
-import n6 from './photos/nature/n6.webp'
-import n7 from './photos/nature/n7.webp'
-import spider from './photos/nature/spider.webp'
-import autumn from './photos/nature/autumn-leaves.webp'
-import bird from './photos/nature/bird.webp'
-import fire from './photos/nature/fire.webp'
-import lake from './photos/nature/lake.webp'
-import mountain from './photos/nature/mountain.webp'
-import sunrise from './photos/nature/sunrise.webp'
-import {useState} from 'react'
-
-const allPictures = [g1,g2,g3,g4,g5,g6,g7,g8 ,n1, n2, n3, n4, n5, n6, n7, spider, autumn, bird, fire, mountain,lake, sunrise]
+import {useState, useEffect} from 'react'
 
 
 function App() {
+  const [links, setLinks] = useState([]);
+ 
+  useEffect(() => {
+    async function getLinks() {
+      const response = await fetch(`http://localhost:5000/links/`);
+  
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+  
+      const links = await response.json();
+      setLinks(links);
+    }
+  
+    getLinks();
+  
+    return;
+  }, [links.length]);
+
+  // console.log(links)
+  const srcLinks = links.map(element => element.link)
+  // console.log(srcLinks)
 	const [hiddenDisplay, setHiddenDisplay] = useState('hidden')
 	const handleSetHiddenDisplay = () => {
 		setHiddenDisplay('flex')
@@ -47,18 +45,18 @@ function App() {
 	}
 
 	const handleImgClick = (event) => {
-		console.log(event)
+		// console.log(event)
 		handleSetHiddenDisplay()
 		handleSetImgSource(event.target.attributes.src.value)
 	}
 
 	const handleClickLeft = () => {
-		const idx = allPictures.indexOf(imgSource)
-		setImgSource(allPictures[idx > 0 ? idx - 1 : allPictures.length - 1])
+		const idx = links.indexOf(imgSource)
+		setImgSource(srcLinks[idx > 0 ? idx - 1 : srcLinks.length - 1])
 	}
 	const handleClickRight = () => {
-		const idx = allPictures.indexOf(imgSource)
-		setImgSource(allPictures[idx < allPictures.length - 1 ? idx + 1 : 0])
+		const idx = srcLinks.indexOf(imgSource)
+		setImgSource(srcLinks[idx < srcLinks.length - 1 ? idx + 1 : 0])
 	}
 
   return (
@@ -68,7 +66,7 @@ function App() {
 			<StickySidebar />
       <HiddenDisplay display={hiddenDisplay} leftClick={handleClickLeft} rightClick={handleClickRight} 
       hide={HideDisplay} show={handleSetHiddenDisplay} imgSource={imgSource}/>
-			<Gallery onClick={handleImgClick} name="Gallery" section={allPictures}/>
+			<Gallery onClick={handleImgClick} name="Gallery" section={srcLinks}/>
 			<Contact />
 		</div>
     	
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
+// const GalleryElement = ({src, onClick}) => {
+// 	return (
+// 			<>
+// 					<img onClick={onClick} alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+// 					src={src} />
+// 			</>
+// 	)
+// }
+// const Els = ({sec, onClick}) => sec.map(elem => <GalleryElement onClick={onClick} src={elem} />)
+
+
+// const Gallery = ({name, section, onClick }) => {
+// 	return (
+// 		<section className="container flex flex-col mx-auto mt-2 overflow-hidden text-gray-700 max-w-mac" id={name}>
+// 			<h2 className='p-3 mt-4 text-2xl font-bold tracking-widest text-center uppercase transition duration-1000 ease-in-out transform border-b-4 border-b-border-dark md:mt-7 lg:mt-10 xl:mt-14 font-roboto sm:text-3xl md:text-4xl lg:text-5xl '>
+// 				{name}
+// 			</h2>
+// 			<div className="container grid grid-cols-2 gap-2 px-2 py-2 mx-auto mt-2 sm:gap-4 lg:gap-6 xl:gap-8 md:grid-cols-3 max lg:pt-12">
+// 				<Els onClick={onClick} sec={section}/>
+// 			</div>
+// 		</section>
+// 	);
+// }
+
 const GalleryElement = ({src, onClick}) => {
 	return (
 			<>
@@ -8,7 +32,7 @@ const GalleryElement = ({src, onClick}) => {
 			</>
 	)
 }
-const Els = ({sec, onClick}) => sec.map(elem => <GalleryElement onClick={onClick} src={elem} />)
+const Els = ({sec, onClick}) => sec.map(elem => <GalleryElement onClick={onClick} key={sec.indexOf(elem)} src={elem} />)
 
 
 const Gallery = ({name, section, onClick }) => {
