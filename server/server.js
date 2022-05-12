53e37b27cc62d5fae0f3fac22cce693d5fc69535
ionut1195 <ionut_tomescu95@yahoo.com>
2022-05-12 13:08:50 +0200

    integrated backend and frontend

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes/links"));
 // get driver connection
+app.use(express.static('../build'))
 const dbo = require("./db/conn");
  
 app.listen(port, () => {
